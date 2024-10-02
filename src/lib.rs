f27fe7115f3b6e9da01c83cb5f8bd8cd46692589
Jessica Schach <schachjessica@gmail.com>
2024-10-02 16:15:05 +0200

    Added Bulma as main style framework.

--- src/lib.rs
+++ src/lib.rs
@@ -26,8 +26,14 @@ pub fn App() -> impl IntoView {
         let title = String::from(meeting.title);
         let date = String::from(meeting.date);
         view! {
-            <div>
-                <span>{ title }</span>" "<span>{ date }</span>
+            <div class="message is-primary">
+                <div class="message-header">
+                    <div class="title is-5">{ title }</div>
+                    <div class="subtitle is-5">{ date }</div>
+                </div>
+                <div class="message-body">
+                    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+                </div>
             </div>
         }
     }).collect::<Vec<_>>();
@@ -35,7 +41,7 @@ pub fn App() -> impl IntoView {
 
         <h1> Hello World! {counter}</h1>
 
-        <button on:click=move |_event| {
+        <button class="button" on:click=move |_event| {
             counter.update(|value| *value = *value + 1);
             log::info!("Button clicked")
         }
