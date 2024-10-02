891765e89428fbad035ea3ecd3cb0ae5a6714d03
Jessica Schach <schachjessica@gmail.com>
2024-10-02 15:57:14 +0200

    Introduced a custom datatype to model Meetings.

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,36 @@
 use leptos::*;
 
+struct Meeting {
+    title: String,
+    date: String
+}
+
+impl Meeting {
+
+    fn new(title: &str, date: &str) -> Self {
+        Self {
+            title: String::from(title),
+            date: String::from(date)
+        }
+    }
+}
 #[component]
 pub fn App() -> impl IntoView {
     let counter = create_rw_signal(0);
-
+    let meetings = vec![
+        Meeting::new("Meeting 1", "01.01.204"),
+        Meeting::new("Meeting 2", "01.01.2028"),
+        Meeting::new("Meeting 3", "24.10.2023")
+    ];
+    let meeting_views = meetings.into_iter().map(|meeting| {
+        let title = String::from(meeting.title);
+        let date = String::from(meeting.date);
+        view! {
+            <div>
+                <span>{ title }</span>" "<span>{ date }</span>
+            </div>
+        }
+    }).collect::<Vec<_>>();
     view! {
 
         <h1> Hello World! {counter}</h1>
@@ -14,5 +41,6 @@ pub fn App() -> impl IntoView {
         }
         > Ok </button>
 
+        { meeting_views }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use leptos::*;
-use rust_leptos_live_coding::App;
+use rust_leptos_playground::App;
 
 fn main() {
-    _ = console_log::init_with_level(log::Level::Debug);
+    let _ = console_log::init_with_level(log::Level::Debug);
     console_error_panic_hook::set_once();
 
     mount_to_body(|| {
